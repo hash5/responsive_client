7c52f21294701283538bef6c0ffba2c4c656f851
Christian Esswein <christian.esswein@student.uibk.ac.at>
2014-03-14 11:46:17 +0100

    added userspecific app storage

--- asset/common/js/hash5/storage/AppData.js
+++ asset/common/js/hash5/storage/AppData.js
@@ -15,3 +15,36 @@ hash5.storage.AppData = function()
 };
 goog.inherits(hash5.storage.AppData, goog.storage.ExpiringStorage);
 goog.addSingletonGetter(hash5.storage.AppData);
+
+/**
+ * returns key prefixed with user id to unsure user specific storage
+ *
+ * @param  {string} key
+ * @return {string}
+ */
+hash5.storage.AppData.prototype.getUserSpecKey = function(key)
+{
+    var userController = hash5.controller.UserController.getInstance(),
+        curUser = userController.getCurrentUser(),
+        userName = 'blaUser'; //curUser.getUserName(); // TODO unsure user is realy set
+
+    return userName + key;
+}
+
+
+/** @override */
+hash5.storage.AppData.prototype.set = function(key, value, opt_expiration)
+{
+    var userKey = this.getUserSpecKey(key);
+
+    return goog.base(this, 'set', userKey, value, opt_expiration);
+};
+
+
+/** @override */
+hash5.storage.AppData.prototype.get = function(key)
+{
+    var userKey = this.getUserSpecKey(key);
+
+    return goog.base(this, 'get', userKey);
+};
