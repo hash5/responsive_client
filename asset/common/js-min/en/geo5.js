a31b32e8ab94468de6ad918d107b12587cf7062f
Christian Esswein <c.esswein@gmail.com>
2014-04-14 20:38:06 +0200

    fixed new client base bath

--- asset/common/js-min/en/geo5.js
+++ asset/common/js-min/en/geo5.js
@@ -1,2 +1,2 @@
 function wr(){O.call(this)}C(wr,ii);wr.prototype.Aa=function(){console.log("geo5 loaded")};hi("geo5",wr);
-//@ sourceURL=/client/asset/common/js-min/LANG/geo5.js
+//@ sourceURL=/asset/common/js-min/LANG/geo5.js
