c289a19c496fa65d8b9db3a58bab807c4d1f5ac9
Christian Esswein <christian.esswein@student.uibk.ac.at>
2014-06-15 16:33:14 +0200

    updated tests to work with current codebase (running plovr dev server is now needed to run tests)

--- asset/deps.bat
+++ asset/deps.bat
@@ -1,6 +1,11 @@
 python closure-library/closure/bin/build/depswriter.py \
     --root_with_prefix="common/js ../../../common/js" \
+    --root_with_prefix="common/templates ../../../common/templates" \
     --root_with_prefix="third-party ../../../third-party" \
     --root_with_prefix="modules/calendar/js ../../../modules/calendar/js" \
+    --root_with_prefix="modules/files/js ../../../modules/files/js" \
+    --root_with_prefix="modules/geo5/js ../../../modules/geo5/js" \
+    --root_with_prefix="modules/intro/js ../../../modules/intro/js" \
     --root_with_prefix="modules/links/js ../../../modules/links/js" \
+    --root_with_prefix="modules/pages/js ../../../modules/pages/js" \
      > common/js/deps.js
